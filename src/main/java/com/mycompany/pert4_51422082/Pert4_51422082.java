b85701747ff4d373defdae72ba1501cad151de6f
heenzu <farizirza77@gmail.com>
2025-11-25 16:35:45 +0700

    first commit

---
+++ src/main/java/com/mycompany/pert4_51422082/Pert4_51422082.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert4_51422082;
+
+/**
+ *
+ * @author Fariz Irza
+ */
+public class Pert4_51422082 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
